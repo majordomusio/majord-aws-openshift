51728560338f2922cf2e5f3c14269f8d4b6e2e43
Michael Kuehl <michael@ratchet.cc>
2018-07-06 21:22:06 +0200

    new inventory for v3.10

--- roles/prepare_inventory/files/deploy.sh
+++ roles/prepare_inventory/files/deploy.sh
@@ -3,11 +3,11 @@
 #
 # Run with:
 #
-# ./install-openshift.sh |& tee install-openshift.log
+# ./deploy.sh |& tee deploy.log
 #
 # 
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
-ansible-playbook -i ~{{instance_user}}/inventory ~{{instance_user}}/openshift-ansible/playbooks/prerequisites.yml
+#ansible-playbook -i ~{{instance_user}}/inventory ~{{instance_user}}/openshift-ansible/playbooks/prerequisites.yml
 ansible-playbook -i ~{{instance_user}}/inventory ~{{instance_user}}/openshift-ansible/playbooks/deploy_cluster.yml
